73f138151af1a11646ffc0dbe404beebf3ed88ba
schan2023 <lamschan996@ufl.edu>
2018-07-26 15:19:35 -0700

    Stored events in firebase, retrieved events by date from firebase

--- LittleThings/Controllers/CreateEventViewController.swift
+++ LittleThings/Controllers/CreateEventViewController.swift
@@ -24,21 +24,45 @@ class CreateEventViewController: UIViewController {
     
     @IBAction func saveButtonPressed(_ sender: Any) {
         
+        //Checks if input field is null
         if inputEventTextField.text == "" {
             let alertErr = Alerts.createAlert(title: "Cannot save empty event!", message: "Please input event.")
             self.present(alertErr, animated: true, completion: nil)
             return
         }
         
+        //Retrieves data from input field
         let eventDescription = inputEventTextField.text
-        let eventDate = convertDateFormatToString(date: Date())
+        let eventDate = "07-25-2018"
         inputEventTextField.resignFirstResponder()
         inputEventTextField.text = ""
         
         //Saves event to Firebase
         ref = Database.database().reference()
         let user = Auth.auth().currentUser
-        ref.child("users").child((user?.uid)!).child("Events").child(eventDate).childByAutoId().setValue(eventDescription)
+        ref.child("EventDays").child(eventDate).child("date").observeSingleEvent(of: .value, with: {(snapshot) -> Void in
+            if snapshot.exists() {
+                print("Snapshot value exists: \(snapshot.value)")
+                let snapValue = snapshot.value as! String
+                if snapValue == eventDate {
+                    print("Add events to current reference: EventDays->currentDate->event")
+                    self.ref.child("EventDays").child(eventDate).childByAutoId().setValue(eventDescription)
+                }
+                else {
+                    print("new item w/ index to first tree, add new node to EventDays->currentDate")
+                    self.ref.child("EventDays").child(eventDate).child("date").setValue(eventDate)
+                    self.updateEventDays(user: user, id: eventDate)
+                }
+            }
+            else {
+                print("Snapshot value does not exist, new item w/ index to first tree, add new node to EventDays->currentDate")
+                self.ref.child("EventDays").child(eventDate).child("date").setValue(eventDate)
+                self.ref.child("EventDays").child(eventDate).childByAutoId().setValue(eventDescription)
+                self.updateEventDays(user: user, id: eventDate)
+            }
+
+        })
+        
         
         let alert = Alerts.createAlert(title: "Event Saved!", message: "")
         self.present(alert, animated: true, completion: nil)
@@ -51,6 +75,20 @@ class CreateEventViewController: UIViewController {
         let dateString = dateformatter.string(from: Date())
         
         return dateString
+    }
+    
+    func updateEventDays(user: User?, id: String) {
+        //happens with every new date
+        ref.child("users").child((user?.uid)!).child("numOfEvents").observeSingleEvent(of: .value, with: {(Snap) in
+            if Snap.exists(){
+                let num = Snap.value as! Int
+                self.ref.child("users").child((user?.uid)!).child("EventDaysKeys").child(String(num+1)).setValue(id)
+                self.ref.child("users").child((user?.uid)!).child("numOfEvents").setValue(num + 1)
+            } else {
+                self.ref.child("users").child((user?.uid)!).child("EventDaysKeys").child(String(1)).setValue(id)
+                self.ref.child("users").child((user?.uid)!).child("numOfEvents").setValue(1)
+            }
+        })
     }
 }
 
--- LittleThings/Controllers/DisplayEventViewController.swift
+++ LittleThings/Controllers/DisplayEventViewController.swift
@@ -8,10 +8,14 @@
 
 import Foundation
 import UIKit
+import FirebaseAuth
+import Firebase
+import FirebaseDatabase
 
 class DisplayEventViewController: UIViewController {
     
     var currentEvent: Event?
+    var arrayOfEvents: [String] = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,18 +29,54 @@ class DisplayEventViewController: UIViewController {
     }
     
     func generateEvent() -> Void {
-        //Retrieve events from core data
-        let eventsArray = CoreDataHelper.retrieveEvents()
-        let num = eventsArray.count
         
-        //Generate random number from 0 or array size
-        let randomInt = Int(arc4random_uniform(UInt32(num)))
+        //Get snapshot of user.uid
+        let ref = Database.database().reference()
+        let user = Auth.auth().currentUser
+        ref.child("users").child((user?.uid)!).observeSingleEvent(of: .value, with: { (snapshot) in
+            let value = snapshot.value as? NSDictionary
+            print(value)
+            
+            //get numOfEvents
+            let numOfEvents = value!["numOfEvents"] as? UInt32
+            print("Number of events: \(numOfEvents)")
+            
+            //random number
+            let randNum = Int(arc4random_uniform(UInt32(numOfEvents!))) + 1
+            print("random number: \(randNum)")
+            
+            //Get value of specific day
+            let foundDateArray = value!["EventDaysKeys"] as? NSArray
+            let foundDate = foundDateArray![randNum] as! String
+            print("This date was found: \(foundDate)")
+            
+            //Get snapshot of EventDays.foundDate
+            ref.child("EventDays").child(foundDate).observeSingleEvent(of: .value, with: { (snap) in
+                print("Retrieve snapshot of EventDate.foundDate")
+                print(snap.value)
+                let snapDict = snap.value as! NSDictionary
+                for (key, value) in snapDict as! [String : String] {
+                    if key != "date" {
+                        self.arrayOfEvents.append(value)
+                    }
+                }
+                print(self.arrayOfEvents)
+            })
+            
+        })
         
-        //Set label as event description
-        eventLabel.text = eventsArray[randomInt].eventDescription
-        
-        //Saves current event
-        currentEvent = eventsArray[randomInt]
+//        //Retrieve events from core data
+//        let eventsArray = CoreDataHelper.retrieveEvents()
+//        let num = eventsArray.count
+//
+//        //Generate random number from 0 or array size
+//        let randomInt = Int(arc4random_uniform(UInt32(num)))
+//
+//        //Set label as event description
+//        eventLabel.text = eventsArray[randomInt].eventDescription
+//
+//        //Saves current event
+//        currentEvent = eventsArray[randomInt]
         
     }
     
